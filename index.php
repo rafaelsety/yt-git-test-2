fde43256be5ea3593149551a4977dbb7eec50540
rafaelsety <mhmmdrafael28@gmail.com>
2023-11-27 13:20:22 +0700

    nambah list ke tiga

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba Git Remote</title>
 </head>
+
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui placeat id dolor? Aliquam quos harum ratione officiis debitis corrupti dignissimos at ut, obcaecati odit. Pariatur neque eius magni eligendi laboriosam.</p>
     <ul>
         <li>satu</li>
         <li>dua</li>
+        <li>tiga</li>
     </ul>
 </body>
+
 </html>
